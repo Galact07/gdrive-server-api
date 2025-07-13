0bcee36904c1cba050c311629c9549821b46aff4
Sidharth Nair <sidharthnair1907@gmail.com>
2025-07-13 23:57:08 +0530

    fix3

--- api/gdrive-images.ts
+++ api/gdrive-images.ts
@@ -1,3 +1,10 @@
-import { handler } from '../src/lib/googleDriveApi';
-
-export default handler; 
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  res.status(200).json({ message: 'CORS test success' });
+} 
