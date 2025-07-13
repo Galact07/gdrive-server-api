15218ab2d20cb5e4acb330a13c0d045f8b8d961c
Sidharth Nair <sidharthnair1907@gmail.com>
2025-07-14 00:11:07 +0530

    fix5

---
+++ api/cors-test.ts
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  res.status(200).json({ message: 'CORS test success' });
+} 
