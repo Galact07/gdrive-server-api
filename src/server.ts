d4e082248d3615c0ec06e00aafd236a3a578520a
Sidharth Nair <sidharthnair1907@gmail.com>
2025-07-13 19:47:49 +0530

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,19 @@ import cors from 'cors';
 import { createGoogleDriveMiddleware, serveGoogleDriveImage } from './lib/googleDriveApi.js';
 
 const app = express();
+
+// Global CORS headers for all responses
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 
